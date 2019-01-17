6b4525f904abac0d74f1bcf535e5d5273d5c29a6
thomasfrl <feral.thomas@gmail.com>
2019-01-17 10:12:50 +0100

    mise en page et dernier test qui ne fonctionne pas

--- lib/twitter_bot.rb
+++ lib/twitter_bot.rb
@@ -3,43 +3,63 @@ require 'twitter'
 require 'pry'
 require_relative 'journalists_list.rb'
 
-Dotenv.load # Ceci appelle le fichier .env grâce à la gem Dotenv, et importe toutes les données enregistrées dans un hash ENV
-
-# Il est ensuite très facile d'appeler les données du fichier .env, par exemple là je vais afficher le contenu de la clé TWITTER_API_SECRET
-#puts ENV['TWITTER_API_SECRET']
-
-# tout est stocké dans un hash qui s'appelle ENV. Tu peux le regarder en faisant :
-#puts ENV
-
- # ligne très importante qui appelle la gem.
-
-# n'oublie pas les lignes pour Dotenv ici…
-
-# quelques lignes qui appellent les clés d'API de ton fichier .env
-client = Twitter::REST::Client.new do |config|
-  config.consumer_key        = ENV["TWITTER_API_KEY"]
-  config.consumer_secret     = ENV["TWITTER_API_SECRET"]
-  config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
-  config.access_token_secret = ENV["TWITTER_ACCESS_TOKEN_SECRET"]
+def methode_connect
+	# quelques lignes qui appellent les clés d'API de ton fichier .env
+	client = Twitter::REST::Client.new do |config|
+	  config.consumer_key        = ENV["TWITTER_API_KEY"]
+	  config.consumer_secret     = ENV["TWITTER_API_SECRET"]
+	  config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
+	  config.access_token_secret = ENV["TWITTER_ACCESS_TOKEN_SECRET"]
+	end
+	return client
 end
 
-random_numbers =  []
-3.times do |i|
-	random_numbers[i] = rand (0..@journalists.size-1)
+def methode_streaming
+	Dotenv.load
+	client = Twitter::Streaming::Client.new do |config|
+	  config.consumer_key        = ENV["TWITTER_API_KEY"]
+	  config.consumer_secret     = ENV["TWITTER_API_SECRET"]
+	  config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
+	  config.access_token_secret = ENV["TWITTER_ACCESS_TOKEN_SECRET"]
+	end
+	#binding.pry
+	return client
 end
-binding.pry
-# ligne qui permet de tweeter sur ton compte
-#client.update('Mon premier tweet en Ruby !!!! #bonjour_monde')
 
+def methode_tweet (client)
+	# ligne qui permet de tweeter sur ton compte
+	client.update('Mon troisième tweet en Ruby !!!! #bonjour_monde')
+end
 
-#random_numbers.each do |nb|
-#	client.update("#{@journalists[nb]} Hey I love Ruby too, what are your favorite blogs? :)")
-#end
+def methode_tweet_journalist(client)
+	random_numbers =  []
+	3.times do |i|
+		random_numbers[i] = rand (0..@journalists.size-1)
+	end
 
+	random_numbers.each do |nb|
+		client.update("#{@journalists[nb]} Hey I love Ruby too, what are your favorite blogs? :)")
+	end
+end
 
-#client.search("#bonjour_monde", result_type: "recent").take(25).each do |tweet|
-#	client.favorite tweet
-#end
-#client.search("#bonjour_monde", result_type: "recent").take(20).each do |tweet|
-#	client.follow tweet.user
-#end
+ def  methode_favorite(client)
+	client.search("#bonjour_monde", result_type: "recent").take(5).each do |tweet|
+		client.favorite tweet
+	end
+end
+
+def methode_follow(client)
+	client.search("#bonjour_monde", result_type: "recent").take(3).each do |tweet|
+		client.follow tweet.user
+	end
+end
+
+def methode_tweet_streaming(client)
+	client.filter(track: "@bonjour_monde") do |tweet|
+	  client.favorite tweet
+	  #client.follow tweet.user
+	  puts "cool un tweet"
+	end
+end
+
+methode_tweet_streaming(methode_streaming)
